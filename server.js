a75206d636494b264963cf0598b697ee0062d730
Eric James Deiter <eric@deiter.com>
2018-10-08 15:53:42 -0400

    WIP nodemon, sequelize

--- server.js
+++ server.js
@@ -6,6 +6,69 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(
+  'database',
+  'username',
+  'password',
+  {
+    dialect: 'sqlite',
+    storage: './db/app.sqlite'
+  }
+)
+
+sequelize
+  .authenticate()
+  .then(
+    () => {
+      console.log('Connection has been established successfully');
+    }
+  )
+  .catch(
+    (err) => {
+      console.log('Unable to connect');
+    }
+  )
+
+const App = sequelize
+  .define(
+    'app',
+    {
+      version: {
+        type: Sequelize.STRING
+      }
+    }
+  )
+
+App.sync(
+    // {force: true}
+  )
+  .then(
+    () => {
+      console.log('Setup App table successfully');
+      return App.create({
+        version: 3
+      })
+    }
+  )
+  .then(
+    () => {
+      return App.findAll().then(users => {
+        console.log(users)
+      })
+
+    }
+  )
+  .catch(
+    (err) => {
+      console.log('Cannot setup App table');
+    }
+  )
+
+
+
+
+
 app.prepare()
 .then(() => {
   const server = express()
